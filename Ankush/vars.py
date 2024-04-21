dd95f0cc7e52108386185adcade704b4b3cf2c9b
piyushcodings <118120770+piyushcodings@users.noreply.github.com>
2024-04-21 06:01:15 +0530

    Update vars.py

--- Ankush/vars.py
+++ Ankush/vars.py
@@ -1,5 +1,5 @@
-api_id = 20491966
-api_hash = "aa1c8f86db7f78fe9bfdd77bb48a5b23"
-bot_token = "6615366987:AAFqURUR5DibftknVctiPgJ_7ZOIjwthmL4"
-AUTH = 5881684718
-#Bot Username = "Anajajbsbsbsbot"
+api_id = 23907288
+api_hash = "f9a47570ed19aebf8eb0f0a5ec1111e5"
+bot_token = "6768524089:AAFVzGkRDQy7_CJmCd63D-4RtHGKuuGFRTs"
+AUTH = 6464246531
+#Bot Username = "PWdownloader2_bot"
